a8b9417a2853b98057d0c51e588255258fec0998
Nuno Cardoso <Nuno.Cardoso@nav.no>
2020-03-25 10:46:44 +0100

    EUX-2111 Merge banner with header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,12 @@
+import { toggleHighContrast } from 'actions/ui'
 import classNames from 'classnames'
 import { State } from 'declarations/reducers'
 import { Saksbehandler } from 'declarations/types'
+import Ui from 'eessi-pensjon-ui'
 import PT from 'prop-types'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import NEESSILogo from 'resources/images/nEESSI'
 
@@ -25,6 +27,12 @@ export const mapState = (state: State): HeaderSelector => ({
 const Header: React.FC<HeaderProps> = ({ className }: HeaderProps): JSX.Element => {
   const { saksbehandler }: HeaderSelector = useSelector<State, HeaderSelector>(mapState)
   const { t } = useTranslation()
+  const dispatch = useDispatch()
+
+  const handleHighContrastToggle = (): void => {
+    dispatch(toggleHighContrast())
+  }
+
   return (
     <header className={classNames(className, 'c-header')}>
       <div className='c-header__brand'>
@@ -34,7 +42,21 @@ const Header: React.FC<HeaderProps> = ({ className }: HeaderProps): JSX.Element 
         <div className='c-header__skillelinje' />
         <div className='c-header__tittel'><span>{t('ui:app-name')}</span></div>
       </div>
+      <Ui.Nav.Undertittel>
+         {t('ui:app-title')}
+      </Ui.Nav.Undertittel>
       <div className='c-header__saksbehandler'>
+        <Ui.Nav.Lenke
+                 className='c-header__highcontrast-link mr-3'
+                  href='#highContrast'
+                  onClick={(e: React.MouseEvent) => {
+                    e.preventDefault()
+                    e.stopPropagation()
+                    handleHighContrastToggle()
+                  }}
+                >
+                  {t('ui:label-highContrast')}
+        </Ui.Nav.Lenke>
         {saksbehandler && saksbehandler.navn ? (
           <div className='saksbehandler__navn'>
             {saksbehandler.navn}
