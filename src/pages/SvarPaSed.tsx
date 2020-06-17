bf7142ade6aa5caff600e3fe1b14ad566ccece72
Nuno Cardoso <Nuno.Cardoso@nav.no>
2020-06-17 13:57:22 +0200

    Scaffolding

---
+++ src/pages/SvarPaSed.tsx
@@ -0,0 +1,15 @@
+import TopContainer from 'components/TopContainer/TopContainer'
+import React from 'react'
+
+const SvarPaSed: React.FC = (): JSX.Element => {
+
+  return (
+    <TopContainer className='p-svarpased'>
+     <div>
+       hello
+     </div>
+    </TopContainer>
+  )
+}
+
+export default SvarPaSed
