a4fd0e91b4f169602012f617e82f7cc1862d8914
Magnus Skalleberg Rom <Magnus.Skalleberg.Rom@nav.no>
2020-06-23 13:24:00 +0200

    Added the select for sed

--- src/actions/svarpased.ts
+++ src/actions/svarpased.ts
@@ -1,17 +1,52 @@
-import * as types from 'constants/actionTypes'
-import * as urls from 'constants/urls'
-import * as api from 'eessi-pensjon-ui/dist/api'
-import { ActionWithPayload, ThunkResult } from 'eessi-pensjon-ui/dist/declarations/types'
-import { ActionCreator } from 'redux'
-const sprintf = require('sprintf-js').sprintf
+import * as types from "constants/actionTypes";
+import * as urls from "constants/urls";
+import * as api from "eessi-pensjon-ui/dist/api";
+import {
+  ActionWithPayload,
+  ThunkResult,
+} from "eessi-pensjon-ui/dist/declarations/types";
+import { ActionCreator } from "redux";
+const sprintf = require("sprintf-js").sprintf;
 
-export const getSaksnummer: ActionCreator<ThunkResult<ActionWithPayload>> = (saksnummer: string): ThunkResult<ActionWithPayload> => {
+export const getSaksnummer: ActionCreator<ThunkResult<ActionWithPayload>> = (
+  saksnummer: string
+): ThunkResult<ActionWithPayload> => {
   return api.realCall({
     url: sprintf(urls.API_SVARPASED_SAKSNUMMER_URL, { saksnummer: saksnummer }),
     type: {
       request: types.SVARPASED_SAKSNUMMER_GET_REQUEST,
       success: types.SVARPASED_SAKSNUMMER_GET_SUCCESS,
-      failure: types.SVARPASED_SAKSNUMMER_GET_FAILURE
-    }
-  })
-}
+      failure: types.SVARPASED_SAKSNUMMER_GET_FAILURE,
+    },
+  });
+};
+
+export const getFnummerDnummer: ActionCreator<ThunkResult<
+  ActionWithPayload
+>> = (fnummerDnummer: string): ThunkResult<ActionWithPayload> => {
+  return api.realCall({
+    url: sprintf(urls.API_SVARPASED_FNUMMERDNUMMER_URL, {
+      fnummerDnummer: fnummerDnummer,
+    }),
+    type: {
+      request: types.SVARPASED_FNUMMERDNUMMER_GET_REQUEST,
+      success: types.SVARPASED_FNUMMERDNUMMER_GET_SUCCESS,
+      failure: types.SVARPASED_FNUMMERDNUMMER_GET_FAILURE,
+    },
+  });
+};
+
+export const getSed: ActionCreator<ThunkResult<ActionWithPayload>> = (
+  sed: string
+): ThunkResult<ActionWithPayload> => {
+  return api.realCall({
+    url: sprintf(urls.API_SVARPASED_SED_URL, {
+      sed: sed,
+    }),
+    type: {
+      request: types.SVARPASED_SED_GET_REQUEST,
+      success: types.SVARPASED_SED_GET_SUCCESS,
+      failure: types.SVARPASED_SED_GET_FAILURE,
+    },
+  });
+};
