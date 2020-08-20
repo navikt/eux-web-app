d82ccaf0f478e8883498b933039a87a968c18982
Nuno Cardoso <Nuno.Cardoso@nav.no>
2020-08-20 10:58:46 +0200

    Inntekt table improvements

--- src/components/Inntekt/InntektsTabell.tsx
+++ src/components/Inntekt/InntektsTabell.tsx
@@ -1,55 +1,35 @@
 import React from "react";
-import "nav-frontend-tabell-style";
 import { Inntekt } from "declarations/types";
 import InntektsTabellRow from "components/Inntekt/InntektsTabellRow";
-//import { Cell } from "components/StyledComponents";
-//import styled from "styled-components";
-import { css } from "@emotion/core";
 
 interface InntektsTabellProps {
   inntekter: Inntekt[] | undefined;
 }
 
-/*
-const TableCell = styled(Cell)`
-  display: flex;
-  background-color: purple;
-`;
-*/
-
-const pStyle = css({
-  backgroundColor: "purple",
-  width: "100%",
-});
-
-//className="tabell"
-
 const InntektsTabell: React.FunctionComponent<InntektsTabellProps> = ({
   inntekter,
 }) => {
   return (
-    <div css={pStyle}>
-      <table className="tabell">
-        <thead>
-          <tr>
-            <th>Fra Dato</th>
-            <th>Til Dato</th>
-            <th>Beløp</th>
-            <th>Type</th>
-          </tr>
-        </thead>
-        <tbody>
-          {inntekter?.map((inntekt: Inntekt) => (
-            <InntektsTabellRow
-              fraDato={inntekt.fraDato}
-              tilDato={inntekt.tilDato}
-              beloep={inntekt.beloep}
-              type={inntekt.type}
-            />
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <table className="tabell">
+      <thead>
+        <tr>
+          <th>Fra Dato</th>
+          <th>Til Dato</th>
+          <th>Beløp</th>
+          <th>Type</th>
+        </tr>
+      </thead>
+      <tbody>
+        {inntekter?.map((inntekt: Inntekt) => (
+          <InntektsTabellRow
+            fraDato={inntekt.fraDato}
+            tilDato={inntekt.tilDato}
+            beloep={inntekt.beloep}
+            type={inntekt.type}
+          />
+        ))}
+      </tbody>
+    </table>
   );
 };
 export default InntektsTabell;
