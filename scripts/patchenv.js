63847eda0f7e463c27e39c8eb756ee92eb39b948
Nuno Cardoso <Nuno.Cardoso@nav.no>
2021-11-22 10:57:50 +0100

    Adding PDU1 samples

--- scripts/patchenv.js
+++ scripts/patchenv.js
@@ -1,18 +1,23 @@
 import moment from 'moment';
 import dotenv from 'dotenv-save';
-import branch from 'git-branch';
 import { readPackageSync } from 'read-pkg';
 // Read package.json into js map.
 const pkg = readPackageSync({cwd: '.'})
+import simpleGit from 'simple-git';
 
 moment.locale('nb');
 
-const buildNumber = process.env.BUILD_NUMBER || 'local';
+const git = simpleGit({
+  baseDir: process.cwd()
+});
+
 const version = `${process.env.npm_package_version}`;
 let branchName = process.env.BRANCH_NAME || 'unknown';
 if (branchName === 'unknown') {
-  branchName = branch.sync(process.cwd());
+  branchName = await git.branch();
 }
+const describe = await git.raw('describe');
+
 const {
   dependencies: {
     'eessi-kodeverk': kodeverk_versjon,
@@ -21,7 +26,7 @@ const {
 } = pkg;
 dotenv.set('REACT_APP_VERSION', version);
 dotenv.set('REACT_APP_BUILD_DATETIME', moment().format('DD/MM/YYYY HH:mm'));
-dotenv.set('REACT_APP_BUILD_VERSION', buildNumber);
-dotenv.set('REACT_APP_BRANCH_NAME', branchName);
+dotenv.set('REACT_APP_BUILD_VERSION', describe);
+dotenv.set('REACT_APP_BRANCH_NAME', branchName.current);
 dotenv.set('REACT_APP_EESSI_KODEVERK', kodeverk_versjon);
 dotenv.set('REACT_APP_REACT_LIB', react_lib_versjon.slice(1));
