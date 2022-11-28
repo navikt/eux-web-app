fd0688b66da9e61c6b1499a560d54714ab71a060
Rittik Dey <dey.rittik@nav.no>
2022-11-28 15:36:17 +0100

    TEN-54: Correct props for PDU1_ASJSON_SUCCESS - use payload

--- src/reducers/pdu1.ts
+++ src/reducers/pdu1.ts
@@ -60,7 +60,7 @@ const pdu1Reducer = (state: Pdu1State = initialPdu1State, action: AnyAction): Pd
     case types.PDU1_ASJSON_SUCCESS: {
       const pdu1: PDU1 = (action as ActionWithPayload).payload
       pdu1.saksreferanse = (action as ActionWithPayload).payload.saksreferanse
-      pdu1.fagsakId = (action as ActionWithPayload).context.fagsak
+      pdu1.fagsakId = (action as ActionWithPayload).payload.fagsakId
       pdu1.__fagsak = (action as ActionWithPayload).context.fagsak
       pdu1.__dokumentId = (action as ActionWithPayload).context.dokumentId
       pdu1.__journalpostId = (action as ActionWithPayload).context.journalpostId
