182a42537a2b70522482bffac3dccc478b9cbce5
Rittik Dey <dey.rittik@nav.no>
2023-07-04 15:12:56 +0200

    TEN-116: Add Participants_Send as allowed sed action

--- src/utils/allowedFeatures.ts
+++ src/utils/allowedFeatures.ts
@@ -7,8 +7,8 @@ const allowed:Allowed = {
   ALLOWED_TO_FILL_OUT: ["H001"],
   ADMIN_ALLOWED_SAKSHANDLINGER: ["H001", "X001", "X009", "F002", "F004", "F022", "F023", "F026", "F027", "F016", "R001", "R003", "R004", "Delete_Case"],
   ALLOWED_SAKSHANDLINGER: ["H001", "X001", "X009", "F002", "Delete_Case"],
-  ADMIN_ALLOWED_SED_HANDLINGER: ["H002", "F002", "U002", "U004", "U017", "X008", "X010", "X011", "X012", "Delete", "Update", "Read", "Send"],
-  ALLOWED_SED_HANDLINGER: ["H002", "F002", "U002", "U004", "U017", "X008", "X010", "X011", "X012", "Delete", "Update", "Read", "Send"],
+  ADMIN_ALLOWED_SED_HANDLINGER: ["H002", "F002", "U002", "U004", "U017", "X008", "X010", "X011", "X012", "Delete", "Update", "Read", "Send", "Participants_Send"],
+  ALLOWED_SED_HANDLINGER: ["H002", "F002", "U002", "U004", "U017", "X008", "X010", "X011", "X012", "Delete", "Update", "Read", "Send", "Participants_Send"],
   ADMIN_ALLOWED_SED_EDIT_AND_UPDATE: ["H001", "H002", "F001", "F002", "U002", "U004", "U017", "X001", "X008", "X009", "X010", "X011", "X012"],
   ALLOWED_SED_EDIT_AND_UPDATE: ["H001", "H002", "F002", "U002", "U004", "U017", "X001", "X008", "X009", "X010", "X011", "X012"]
 }
