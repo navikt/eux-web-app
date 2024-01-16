6e4642176e399c6e1c9e2ac27e1d44ff11acb0dc
Rittik Dey <dey.rittik@nav.no>
2024-01-16 14:27:57 +0100

    TEN-313: Add mockdata

--- src/mocks/journalfoeringFagsakList.ts
+++ src/mocks/journalfoeringFagsakList.ts
@@ -1,5 +1,34 @@
 const fagsaker = [
-
+  {
+    "aktoerId": "140274798",
+    "id": "140274798",
+    "tema": "DAG",
+    "type": "GENERELL_SAK",
+    "system": "FS22"
+  },
+  {
+   "id": "140279256",
+   "aktoerId": "140274798",
+   "tema": "DAG",
+   "type": "FAGSAK",
+   "nr": "0331D17",
+   "system": "IT01"
+  },
+  {
+    "id": "140272690",
+    "aktoerId": "140274798",
+    "tema": "BAR",
+    "type": "GENERELL_SAK",
+    "system": "FS22"
+  },
+  {
+    "id": "140282517",
+    "tema": "KON",
+    "type": "FAGSAK",
+    "nr": "1/2023",
+    "system": "AO11",
+    "fnr": "12345678901"
+  }
 ]
 
 export const mockJournalfoeringFagsaker = () => {
