69eebd96095587fcc6de06fb5b6668c49bd39347
Rittik Dey <dey.rittik@nav.no>
2024-04-03 15:44:52 +0200

    TEN-677: Rollback to Aksel V5.  Remove dotenv.set('GENERATE_SOURCEMAP', false)

--- scripts/patchenv.js
+++ scripts/patchenv.js
@@ -30,4 +30,3 @@ dotenv.set('REACT_APP_BUILD_VERSION', describe);
 dotenv.set('REACT_APP_BRANCH_NAME', branchName.current);
 dotenv.set('REACT_APP_EESSI_KODEVERK', kodeverk_versjon);
 dotenv.set('REACT_APP_REACT_LIB', react_lib_versjon.slice(1));
-dotenv.set('GENERATE_SOURCEMAP', false);
