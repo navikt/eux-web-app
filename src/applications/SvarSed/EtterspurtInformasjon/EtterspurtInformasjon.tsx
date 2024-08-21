953f8a56aaf2a930dea3d4964687dcbde1ff91a8
Rittik Dey <dey.rittik@nav.no>
2024-08-21 11:11:30 +0200

    TEN-852: EtterspurtInformasjonsTyper as separate common component. Add fields for all etterspurt info types

--- src/applications/SvarSed/EtterspurtInformasjon/EtterspurtInformasjon.tsx
+++ src/applications/SvarSed/EtterspurtInformasjon/EtterspurtInformasjon.tsx
@@ -1,14 +1,15 @@
 import {State} from "declarations/reducers";
 import {MainFormProps, MainFormSelector} from "../MainForm";
-import React, {useEffect} from "react";
+import React from "react";
 import {useAppDispatch, useAppSelector} from "store";
-import {UNSAFE_Combobox, Box, Checkbox, Heading, VStack} from "@navikt/ds-react";
+import {Box, Checkbox, Heading, VStack} from "@navikt/ds-react";
 import {PaddedDiv} from "@navikt/hoykontrast";
 import useUnmount from "hooks/useUnmount";
-import { useState } from 'react'
 import _ from "lodash";
 import {AnmodningOmMerInformasjon} from "declarations/sed";
-import {ComboboxOption} from "@navikt/ds-react/cjs/form/combobox/types";
+import EtterspurtInformasjonTyper from "./EtterspurtInformasjonTyper";
+import TextArea from "../../../components/Forms/TextArea";
+import {useTranslation} from "react-i18next";
 
 const mapState = (state: State): MainFormSelector => ({
   validation: state.validation.status
@@ -21,25 +22,15 @@ const EtterspurtInformasjon: React.FC<MainFormProps> = ({
   updateReplySed,
   //personName
 }: MainFormProps): JSX.Element => {
-  //const {t} = useTranslation()
+  const {t} = useTranslation()
   const {validation}: MainFormSelector = useAppSelector(mapState)
   const dispatch = useAppDispatch()
 
   const namespace = `${parentNamespace}-etterspurtinformasjon`
   const target = 'anmodningOmMerInformasjon'
   const anmodningOmMerInformasjon: AnmodningOmMerInformasjon | undefined = _.get(replySed, target)
-  let initialSelectedOptions:ComboboxOption[] = []
 
   console.log(namespace, anmodningOmMerInformasjon)
-
-
-  useEffect(() => {
-    anmodningOmMerInformasjon?.adopsjon?.etterspurtInformasjonType?.typer?.forEach((type) => {
-      // @ts-ignore
-      const option: ComboboxOption = initialOptions?.find((o) => o.value === type)
-      initialSelectedOptions.push(option)
-    })
-  }, [])
 
   useUnmount(() => {
     const clonedValidation = _.cloneDeep(validation)
@@ -50,29 +41,47 @@ const EtterspurtInformasjon: React.FC<MainFormProps> = ({
     dispatch(updateReplySed(`${target}.${e.target.value}`, e.target.checked ? {} : undefined))
   }
 
-  const [selectedOptions, setSelectedOptions] = useState<ComboboxOption[]>(initialSelectedOptions);
-  const onToggleSelected = (option: string, isSelected: boolean) => {
-    if (isSelected) {
-      // @ts-ignore
-      setSelectedOptions([...selectedOptions, initialOptions?.find((o) => o.value === option)]);
-    } else {
-      setSelectedOptions(selectedOptions.filter((o) => o?.value !== option));
-    }
-  };
+  const setYtterligereInfo = (type:string, value: string) => {
+    dispatch(updateReplySed(`${target}.${type}.ytterligereInformasjon`, value))
+  }
 
-  const initialOptions = [
-    {label: "Dato da adoptivforeldrene fikk_omsorg for det adopterte barnet", value: 'dato_da_adoptivforeldrene_fikk_omsorg_for_det_adopterte_barnet'},
+  const etterspurtInformasjonsTyperAdopsjon = [
+    {label: "Dato da adoptivforeldrene fikk omsorg for det adopterte barnet", value: 'dato_da_adoptivforeldrene_fikk_omsorg_for_det_adopterte_barnet'},
     {label: "Dato da adopsjonsbevillingen ble offentlig registrert", value: 'dato_da_adopsjonsbevillingen_ble_offentlig_registrert'},
     {label: "Dokument som stadfester at adopsjonen er lovlig", value: 'dokument_som_stadfester_at_adopsjonen_er_lovlig'}
   ]
 
-  useEffect(() => {
-    const etterspurtInformasjonTypeArray: Array<string> = []
-    selectedOptions.forEach((o) => {
-      etterspurtInformasjonTypeArray.push(o.value)
-    })
-    dispatch(updateReplySed(`${target}.adopsjon.etterspurtInformasjonType.typer`, etterspurtInformasjonTypeArray))
-  }, [selectedOptions])
+  const etterspurtInformasjonsTyperInntekt = [
+    {label: "Type påkrevde data inntektskilde", value: "type_påkrevde_data_inntektskilde"},
+    {label: "Årlig inntekt", value: "årlig_inntekt"},
+    {label: "Periode fratil det kreves opplysninger om", value: "periode_fratil_det_kreves_opplysninger_om"}
+  ]
+
+  const etterspurtInformasjonsTyperYtelseTilForeldreLoese = [
+    {label: "Identifisering av den avdøde", value: "identifisering_av_den_avdøde"},
+    {label: "Identifisering av de berørte barna", value: "identifisering_av_de_berørte_barna"},
+    {label: "Identifikasjon av andre personer en annen slektning verge som søker på vegne av den foreldreløse barnet", value: "identifikasjon_av_andre_personer_en_annen_slektning_verge_som_søker_på_vegne_av_den_foreldreløse_barnet"},
+    {label: "Den foreldreløses barnets bosted", value: "den foreldreløses barnets bosted"},
+    {label: "Relasjon mellom den foreldreløse barnet og avdøde", value: "relasjon_mellom_den_foreldreløse_barnet_og_avdøde"},
+    {label: "Relasjon mellom annen person og den avdøde", value: "relasjon_mellom_annen_person_og_den_avdøde"},
+    {label: "Den foreldreløses barnets aktivitet", value: "den_foreldreløses_barnets_aktivitet"},
+    {label: "Skole", value: "skole"},
+    {label: "Opplæring", value: "opplæring"},
+    {label: "Uførhet", value: "uførhet"},
+    {label: "Arbeidsledighet", value: "qrbeidsledighet"},
+    {label: "Inntekt til den foreldreløse barn", value: "inntekt_til_den_foreldreløse_barn"}
+  ]
+
+  const etterspurtInformasjonsTyperAnnenInformasjonOmBarnet = [
+    {label: "Hvem har daglig omsorg for barnet", value: "hvem_har_daglig_omsorg_for_barnet"},
+    {label: "Hvem har foreldreansvar for barnet", value: "hvem_har_foreldreansvar_for_barnet"},
+    {label: "Er barnet adoptert", value: "er_barnet_adoptert"},
+    {label: "Forsørges barnet av det offentlige", value: "forsørges_barnet_av_det_offentlige"},
+    {label: "Går barnet i barnehage finansieres barnehagen av staten eller det offentlige antall timer barnet går i barnehage", value: "går_barnet_i_barnehage_finansieres_barnehagen_av_staten_eller_det_offentlige_antall_timer_barnet_går_i_barnehage"},
+    {label: "Barnets sivilstand", value: "barnets_sivilstand"},
+    {label: "Dato for endrede forhold", value: "dato_for_endrede_forhold"}
+  ]
+
 
   return (
     <>
@@ -90,15 +99,22 @@ const EtterspurtInformasjon: React.FC<MainFormProps> = ({
               Ved adopsjon
             </Checkbox>
             {!!anmodningOmMerInformasjon?.adopsjon &&
-              <>
-                <UNSAFE_Combobox
-                  label="Etterspurt informasjon"
-                  isMultiSelect
-                  onToggleSelected={onToggleSelected}
-                  selectedOptions={selectedOptions}
-                  options={initialOptions}
+              <VStack gap="4">
+                <EtterspurtInformasjonTyper
+                  target="anmodningOmMerInformasjon.adopsjon"
+                  initialOptions={etterspurtInformasjonsTyperAdopsjon}
+                  updateReplySed={updateReplySed}
                 />
-              </>
+                <TextArea
+                  maxLength={255}
+                  error={validation[namespace + '-adopsjon-yttterligereinformasjon']?.feilmelding}
+                  namespace={namespace}
+                  id='adopsjon-yttterligereinformasjon'
+                  label={t('label:ytterligere-informasjon')}
+                  onChanged={(v) => setYtterligereInfo("adopsjon", v)}
+                  value={anmodningOmMerInformasjon.adopsjon.ytterligereInformasjon ?? ''}
+                />
+              </VStack>
             }
           </Box>
           <Box padding="4" background="surface-subtle" borderWidth="1" borderColor="border-subtle">
@@ -109,6 +125,24 @@ const EtterspurtInformasjon: React.FC<MainFormProps> = ({
             >
               Inntekt
             </Checkbox>
+            {!!anmodningOmMerInformasjon?.inntekt &&
+              <VStack gap="4">
+                <EtterspurtInformasjonTyper
+                  target="anmodningOmMerInformasjon.inntekt"
+                  initialOptions={etterspurtInformasjonsTyperInntekt}
+                  updateReplySed={updateReplySed}
+                />
+                <TextArea
+                  maxLength={255}
+                  error={validation[namespace + '-inntekt-yttterligereinformasjon']?.feilmelding}
+                  namespace={namespace}
+                  id='inntekt-yttterligereinformasjon'
+                  label={t('label:ytterligere-informasjon')}
+                  onChanged={(v) => setYtterligereInfo("inntekt", v)}
+                  value={anmodningOmMerInformasjon.inntekt.ytterligereInformasjon ?? ''}
+                />
+              </VStack>
+            }
           </Box>
           <Box padding="4" background="surface-subtle" borderWidth="1" borderColor="border-subtle">
             <Checkbox
@@ -118,6 +152,24 @@ const EtterspurtInformasjon: React.FC<MainFormProps> = ({
             >
               Ytelse til foreldreløse
             </Checkbox>
+            {!!anmodningOmMerInformasjon?.ytelseTilForeldreLoese &&
+              <VStack gap="4">
+                <EtterspurtInformasjonTyper
+                  target="anmodningOmMerInformasjon.ytelseTilForeldreLoese"
+                  initialOptions={etterspurtInformasjonsTyperYtelseTilForeldreLoese}
+                  updateReplySed={updateReplySed}
+                />
+                <TextArea
+                  maxLength={255}
+                  error={validation[namespace + '-ytelseTilForeldreLoese-yttterligereinformasjon']?.feilmelding}
+                  namespace={namespace}
+                  id='ytelseTilForeldreLoese-yttterligereinformasjon'
+                  label={t('label:ytterligere-informasjon')}
+                  onChanged={(v) => setYtterligereInfo("ytelseTilForeldreLoese", v)}
+                  value={anmodningOmMerInformasjon.ytelseTilForeldreLoese.ytterligereInformasjon ?? ''}
+                />
+              </VStack>
+            }
           </Box>
           <Box padding="4" background="surface-subtle" borderWidth="1" borderColor="border-subtle">
             <Checkbox
@@ -127,6 +179,24 @@ const EtterspurtInformasjon: React.FC<MainFormProps> = ({
             >
               Annen informasjon angående barnet
             </Checkbox>
+            {!!anmodningOmMerInformasjon?.annenInformasjonOmBarnet &&
+              <VStack gap="4">
+                <EtterspurtInformasjonTyper
+                  target="anmodningOmMerInformasjon.annenInformasjonOmBarnet"
+                  initialOptions={etterspurtInformasjonsTyperAnnenInformasjonOmBarnet}
+                  updateReplySed={updateReplySed}
+                />
+                <TextArea
+                  maxLength={255}
+                  error={validation[namespace + '-annenInformasjonOmBarnet-yttterligereinformasjon']?.feilmelding}
+                  namespace={namespace}
+                  id='annenInformasjonOmBarnet-yttterligereinformasjon'
+                  label={t('label:ytterligere-informasjon')}
+                  onChanged={(v) => setYtterligereInfo("ytelseTilForeldreLoese", v)}
+                  value={anmodningOmMerInformasjon.annenInformasjonOmBarnet.ytterligereInformasjon ?? ''}
+                />
+              </VStack>
+            }
           </Box>
           <Box padding="4" background="surface-subtle" borderWidth="1" borderColor="border-subtle">
             <Checkbox
---
+++ src/applications/SvarSed/EtterspurtInformasjon/EtterspurtInformasjonTyper.tsx
@@ -0,0 +1,65 @@
+import React, {useEffect, useState} from "react";
+import {useAppDispatch} from "../../../store";
+import {ComboboxOption} from "@navikt/ds-react/cjs/form/combobox/types";
+import _ from "lodash";
+import replySed from "../../../mocks/svarsed/replySed";
+import {EtterspurtInformasjon} from "declarations/sed";
+import {ActionWithPayload} from "@navikt/fetch";
+import {UpdateReplySedPayload} from "../../../declarations/types";
+import {UNSAFE_Combobox} from "@navikt/ds-react";
+
+interface EtterspurtInformasjonTyperProps {
+  target: string
+  initialOptions: ComboboxOption[]
+  updateReplySed: (needle: string, value: any) => ActionWithPayload<UpdateReplySedPayload>
+}
+
+const EtterspurtInformasjonTyper: React.FC<EtterspurtInformasjonTyperProps> = ({
+  target,
+  initialOptions,
+  updateReplySed
+}: EtterspurtInformasjonTyperProps): JSX.Element => {
+  const dispatch = useAppDispatch()
+  const etterspurtInformasjon: EtterspurtInformasjon | undefined = _.get(replySed, target)
+
+  let initialSelectedOptions:ComboboxOption[] = []
+  const [selectedOptions, setSelectedOptions] = useState<ComboboxOption[]>(initialSelectedOptions);
+
+  useEffect(() => {
+      etterspurtInformasjon?.etterspurtInformasjonType?.typer?.forEach((type) => {
+      // @ts-ignore
+      const option: ComboboxOption = initialOptions?.find((o) => o.value === type)
+      initialSelectedOptions.push(option)
+    })
+  }, [])
+
+  useEffect(() => {
+    const etterspurtInformasjonTypeArray: Array<string> = []
+    selectedOptions.forEach((o) => {
+      etterspurtInformasjonTypeArray.push(o.value)
+    })
+    dispatch(updateReplySed(`${target}.etterspurtInformasjonType.typer`, etterspurtInformasjonTypeArray))
+  }, [selectedOptions])
+
+
+  const onToggleSelected = (option: string, isSelected: boolean) => {
+    if (isSelected) {
+      // @ts-ignore
+      setSelectedOptions([...selectedOptions, initialOptions?.find((o) => o.value === option)]);
+    } else {
+      setSelectedOptions(selectedOptions.filter((o) => o?.value !== option));
+    }
+  };
+
+  return(
+    <UNSAFE_Combobox
+      label="Etterspurt informasjon"
+      isMultiSelect
+      onToggleSelected={onToggleSelected}
+      selectedOptions={selectedOptions}
+      options={initialOptions}
+    />
+  )
+}
+
+export default EtterspurtInformasjonTyper
