85bf7fc38caaf5b6fa9b991f86ac8cf853b7896f
Rittik Dey <dey.rittik@nav.no>
2024-12-18 14:05:03 +0100

    TEN-967: New endpoints for PDU1 and landkode in iso3

--- src/applications/PDU1/Person/Statsborgerskap/Statsborgerskap.tsx
+++ src/applications/PDU1/Person/Statsborgerskap/Statsborgerskap.tsx
@@ -41,9 +41,11 @@ import {setStatsborgerskapModalShown} from "../../../../actions/pdu1";
 
 export interface StatsborgerskapSelector {
   statsborgerskapModalShown: boolean
+  countryCodeMap: {key?: string} | null | undefined
 }
 const mapStatsborgerskapState = (state: State): StatsborgerskapSelector => ({
-  statsborgerskapModalShown: state.pdu1.statsborgerskapModalShown
+  statsborgerskapModalShown: state.pdu1.statsborgerskapModalShown,
+  countryCodeMap: state.app.countryCodeMap
 })
 
 const mapState = (state: State): MainFormSelector => ({
@@ -57,7 +59,7 @@ const Statsborgerskap: React.FC<MainFormProps> = ({
 }:MainFormProps): JSX.Element => {
   const { t } = useTranslation()
   const { validation } = useAppSelector(mapState)
-  const { statsborgerskapModalShown } = useAppSelector(mapStatsborgerskapState)
+  const { statsborgerskapModalShown, countryCodeMap } = useAppSelector(mapStatsborgerskapState)
 
   const dispatch = useAppDispatch()
   const target = 'bruker.statsborgerskap'
@@ -179,6 +181,8 @@ const Statsborgerskap: React.FC<MainFormProps> = ({
     const inEditMode = index < 0 || _editIndex === index
     const _statsborgerskap = index < 0 ? _newStatsborgerskap : (inEditMode ? _editStatsborgerskap : statsborgerskap)
 
+    const country = countryData.findByValue3(statsborgerskap)
+
     if(!inEditMode && !statsborgerskap) return
     return (
       <RepeatableRow
@@ -206,9 +210,10 @@ const Statsborgerskap: React.FC<MainFormProps> = ({
                   hideLabel={false}
                   includeList={CountryFilter.STANDARD({ useUK: false })}
                   menuPortalTarget={document.body}
-                  onOptionSelected={(e: Country) => onStatsborgerskapSelected(e.value, index)}
+                  onOptionSelected={(e: Country) => onStatsborgerskapSelected(e.value3, index)}
                   required
                   values={_statsborgerskap}
+                  useAlpha3Value={true}
                 />
                 )
               : (
@@ -217,9 +222,9 @@ const Statsborgerskap: React.FC<MainFormProps> = ({
                     error={_v[_namespace + '-statsborgerskap']?.feilmelding}
                   >
                     <FlexCenterDiv>
-                      <Flag size='S' country={_statsborgerskap ?? ''} />
+                      <Flag size='S' country={country ? country.value : "XU"} />
                       <HorizontalSeparatorDiv />
-                      {countryData.findByValue(_statsborgerskap)?.label ?? _statsborgerskap}
+                      {country ? country.label : countryCodeMap && _statsborgerskap ? countryCodeMap[_statsborgerskap as keyof typeof countryCodeMap] : _statsborgerskap}
                     </FlexCenterDiv>
                   </FormText>
                 )}
--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -63,8 +63,16 @@ export const API_JOARK_GET_URL = API_BASE_URL + '/vedlegg/dokument/%(journalpost
 export const API_JOARK_ATTACHMENT_URL = API_BASE_URL + '/vedlegg/dokument/%(journalpostId)s/%(dokumentInfoId)s/%(variantformat)s/rina/%(rinaId)s/%(rinaDokumentId)s/%(filnavn)s'
 
 // PD U1
-export const PDU1_GET_URL = API_BASE_URL + '/dokument/%(journalpostId)s/%(dokumentId)s/%(variant)s' // GET
-export const PDU1_JOURNALPOST_URL = API_BASE_URL + '/pdu1/pdf/journalpost' // POST
-export const PDU1_INFO_URL = API_BASE_URL + '/person/%(fnr)s/info/pdu1' // GET
+//export const PDU1_INFO_URL = API_BASE_URL + '/person/%(fnr)s/info/pdu1' // GET (1)
+export const PDU1_INFO_URL = API_BASE_URL + '/person/%(fnr)s/pdu1/innhold' // GET (1)
+
+//export const PDU1_PREVIEW_URL = V2_BASE_URL + '/pdu1/pdf/utkast' // POST (2)
+export const PDU1_PREVIEW_URL = API_BASE_URL + '/pdu1/pdf/utkast' // POST (2)
+
+//export const PDU1_JOURNALPOST_URL = API_BASE_URL + '/pdu1/pdf/journalpost' // POST (3)
+export const PDU1_JOURNALPOST_URL = API_BASE_URL + '/pdu1/journalpost' // POST (3)
+
+//export const PDU1_GET_URL = API_BASE_URL + '/dokument/%(journalpostId)s/%(dokumentId)s/%(variant)s' // GET (4)
+export const PDU1_GET_URL = API_BASE_URL + '/pdu1/journalpost/%(journalpostId)s/dokumentInfoId/%(dokumentId)s/json' // GET (4)
+
 export const PDU1_SEARCH_URL = API_BASE_URL + '/person/%(fnr)s/dokumenter?dokumenttype=DAG_EOS_U1' // GET
-export const PDU1_PREVIEW_URL = V2_BASE_URL + '/pdu1/pdf/utkast' // POST
