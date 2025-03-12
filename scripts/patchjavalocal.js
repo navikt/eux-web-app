dbc0af3cb099fa2b10cc9ce430eab8b90105954d
Rittik Dey <dey.rittik@nav.no>
2025-03-12 13:48:49 +0100

    TEN-1084: Fix ENV variables - REACT_APP --> VITE_APP

--- scripts/patchjavalocal.js
+++ scripts/patchjavalocal.js
@@ -18,10 +18,10 @@ const { proxy } = pkg;
 const DEFAULT_JAVA_SERVER = 'http://localhost:8080';
 
 // update .env with custom version
-dotenv.set('REACT_APP_BUILD_VERSION', 'java_local');
-dotenv.set('REACT_APP_LOCAL_CONTEXT', '/eux');
-dotenv.set('REACT_APP_API_BASE_URL', '/eux/api/');
-dotenv.set('REACT_APP_JAVA_LOCAL_HOST', DEFAULT_JAVA_SERVER);
+dotenv.set('VITE_APP_BUILD_VERSION', 'java_local');
+dotenv.set('VITE_APP_LOCAL_CONTEXT', '/eux');
+dotenv.set('VITE_APP_API_BASE_URL', '/eux/api/');
+dotenv.set('VITE_APP_JAVA_LOCAL_HOST', DEFAULT_JAVA_SERVER);
 
 proxy['/api'].target = DEFAULT_JAVA_SERVER;
 proxy['/frontendlogger'].target = DEFAULT_JAVA_SERVER;
