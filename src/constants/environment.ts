6d44591f6c1a21c0fd508d881154ebed456d02df
Rittik Dey <dey.rittik@nav.no>
2025-03-12 13:22:20 +0100

    TEN-1084: process.env --> import.meta.env Added SVGR plugin to vite.config

--- src/constants/environment.ts
+++ src/constants/environment.ts
@@ -1,7 +1,7 @@
-export default process.env.NODE_ENV
-export const IS_DEVELOPMENT = process.env.NODE_ENV === 'development'
-export const IS_PRODUCTION = process.env.NODE_ENV === 'production'
-export const IS_TEST = process.env.NODE_ENV === 'test'
+export default import.meta.env.NODE_ENV
+export const IS_DEVELOPMENT = import.meta.env.NODE_ENV === 'development'
+export const IS_PRODUCTION = import.meta.env.NODE_ENV === 'production'
+export const IS_TEST = import.meta.env.NODE_ENV === 'test'
 
 export const IS_Q = window.location.hostname.endsWith('preprod.local')
 export const IS_P = window.location.hostname.endsWith('adeo.no')
