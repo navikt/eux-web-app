9a1cc8acd9d265638b16013f20b811b4adce4426
Rittik Dey <dey.rittik@nav.no>
2025-04-28 11:35:58 +0200

    Add F027, F026, S040 to previewableSed

--- src/utils/sed.ts
+++ src/utils/sed.ts
@@ -49,7 +49,7 @@ export const isX011Sed = (replySed: ReplySed | PDU1 | null | undefined): boolean
 export const isX012Sed = (replySed: ReplySed | PDU1 | null | undefined): boolean => (replySed as ReplySed)?.sedType === 'X012'
 
 export const isPreviewableSed = (sedType: string): boolean =>
-  ['F001', 'F002', 'F003', 'H001', 'H002',  'U002', 'U004', 'U017', 'X001', 'X008', 'X009', 'X010', 'X011', 'X012'].indexOf(sedType) >= 0
+  ['F001', 'F002', 'F003', 'F026', 'F027', 'H001', 'H002',  'S040', 'U002', 'U004', 'U017', 'X001', 'X008', 'X009', 'X010', 'X011', 'X012'].indexOf(sedType) >= 0
 
 export const cleanReplySed = (replySed: ReplySed): ReplySed => {
   const newReplySed = _.cloneDeep(replySed)
