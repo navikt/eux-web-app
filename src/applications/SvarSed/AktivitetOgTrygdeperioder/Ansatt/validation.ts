a6b6dd6165c995b1573e2ff1b857363ca1818ed3
Rittik Dey <dey.rittik@nav.no>
2025-05-14 14:18:27 +0200

    TEN-1053: Hoykontrast --> Aksel

---
+++ src/applications/SvarSed/AktivitetOgTrygdeperioder/Ansatt/validation.ts
@@ -0,0 +1,67 @@
+import { validatePeriode } from 'components/Forms/validation'
+import { PensjonPeriode, Periode } from 'declarations/sed'
+import { Validation } from 'declarations/types'
+import _ from 'lodash'
+import { getIdx } from 'utils/namespace'
+import { checkIfDuplicate } from 'utils/validation'
+
+export interface ValidationAnsattPeriodeProps {
+  periode: Periode | undefined,
+  perioder: Array<Periode> | undefined,
+  index?: number
+  personName?: string
+}
+
+export interface ValidationAnsattPerioderProps {
+  perioder: Array<Periode> | undefined
+  personName?: string
+}
+
+export const validateAnsattPeriode = (
+  v: Validation,
+  namespace: string,
+  {
+    periode,
+    perioder,
+    index,
+    personName
+  }: ValidationAnsattPeriodeProps
+): boolean => {
+  const idx = getIdx(index)
+  const hasErrors: Array<boolean> = []
+
+  hasErrors.push(validatePeriode(v, namespace, {
+    periode,
+    index,
+    personName
+  }))
+
+  if (!_.isEmpty(periode?.startdato)) {
+    hasErrors.push(checkIfDuplicate(v, {
+      needle: periode,
+      haystack: perioder,
+      matchFn: (p: Periode) => p.startdato === periode?.startdato && p.sluttdato === periode?.sluttdato,
+      id: namespace + idx + '-startdato',
+      index,
+      message: 'validation:duplicateStartdato',
+      personName
+    }))
+  }
+
+  return hasErrors.find(value => value) !== undefined
+}
+
+export const validateAnsattPerioder = (
+  v: Validation,
+  namespace: string,
+  {
+    perioder,
+    personName
+  }: ValidationAnsattPerioderProps
+): boolean => {
+  const hasErrors: Array<boolean> = []
+  perioder?.forEach((periode: Periode | PensjonPeriode, index: number) => {
+    hasErrors.push(validateAnsattPeriode(v, namespace, { periode: (periode as Periode), perioder, index, personName }))
+  })
+  return hasErrors.find(value => value) !== undefined
+}
