de30fa830902509a46411003ba48879bdce024fc
Rittik Dey <dey.rittik@nav.no>
2025-06-03 09:57:48 +0200

    TEN-1053/TEN-1218: Clean up - remove replaced code for F001/F002 - Trygdeordning only used by F026

--- src/applications/SvarSed/Trygdeordning/Trygdeordning.tsx
+++ src/applications/SvarSed/Trygdeordning/Trygdeordning.tsx
@@ -1,9 +1,8 @@
 import { Heading } from '@navikt/ds-react'
-import { PaddedDiv, VerticalSeparatorDiv } from '@navikt/hoykontrast'
+import { PaddedDiv } from '@navikt/hoykontrast'
 import { setValidation } from 'actions/validation'
 import { MainFormProps, MainFormSelector } from 'applications/SvarSed/MainForm'
 import DekkedePerioder from 'applications/SvarSed/Trygdeordning/DekkedePerioder'
-import FamilieYtelser from 'applications/SvarSed/Trygdeordning/FamilieYtelser'
 import { validateTrygdeordninger, ValidateTrygdeordningerProps } from 'applications/SvarSed/Trygdeordning/validation'
 import { State } from 'declarations/reducers'
 import useUnmount from 'hooks/useUnmount'
@@ -13,7 +12,6 @@ import { useTranslation } from 'react-i18next'
 import { useAppDispatch, useAppSelector } from 'store'
 import performValidation from 'utils/performValidation'
 import { ReplySed } from 'declarations/sed'
-import {isF001Sed, isF002Sed, isF026Sed} from 'utils/sed'
 import RettTilYtelserFSED from '../RettTilYtelserFSED/RettTilYtelserFSED'
 
 const mapState = (state: State): MainFormSelector => ({
@@ -62,37 +60,14 @@ const Trygdeordning: React.FC<MainFormProps> = ({
         setReplySed={setReplySed}
         validation={validation}
       />
-      {/*TODO: REMOVE THIS BLOCK OF CODE WHEN AKTIVITETOGTRYGDEPERIODER IS DONE*/}
-      {(isF001Sed(replySed) || isF002Sed(replySed)) &&
-        <>
-          <PaddedDiv>
-            <Heading size='small'>
-              {t('label:trygdeordningen-familieYtelse')}
-            </Heading>
-          </PaddedDiv>
-          <VerticalSeparatorDiv/>
-          <FamilieYtelser
-            parentNamespace={namespace}
-            personID={personID}
-            personName={personName}
-            replySed={replySed}
-            updateReplySed={updateReplySed}
-            setReplySed={setReplySed}
-            validation={validation}
-          />
-        </>
-      }
-
-      {isF026Sed(replySed) &&
-        <RettTilYtelserFSED
-          parentNamespace={namespace}
-          personID={personID}
-          personName={personName}
-          replySed={replySed}
-          updateReplySed={updateReplySed}
-          setReplySed={setReplySed}
-        />
-      }
+      <RettTilYtelserFSED
+        parentNamespace={namespace}
+        personID={personID}
+        personName={personName}
+        replySed={replySed}
+        updateReplySed={updateReplySed}
+        setReplySed={setReplySed}
+      />
     </>
   )
 }
--- src/applications/SvarSed/Trygdeordning/validation.ts
+++ src/applications/SvarSed/Trygdeordning/validation.ts
@@ -1,5 +1,5 @@
 import { validatePeriode } from 'components/Forms/validation'
-import { PensjonPeriode, Periode, ReplySed } from 'declarations/sed'
+import { Periode, ReplySed } from 'declarations/sed'
 import { Validation } from 'declarations/types'
 import _ from 'lodash'
 import { getNSIdx } from 'utils/namespace'
@@ -19,13 +19,6 @@ export interface ValidateTrygdeordningerProps {
   replySed: ReplySed
   personID: string
   personName: string | undefined
-}
-
-export interface ValidationFamilieytelsePeriodeProps {
-  periode: Periode | PensjonPeriode | undefined,
-  perioder: Array<Periode | PensjonPeriode> | undefined
-  nsIndex?: string,
-  personName?: string
 }
 
 export const validateDekkedePeriode = (
@@ -76,124 +69,34 @@ export const validateDekkedePeriode = (
   return hasErrors.find(value => value) !== undefined
 }
 
-export const validateFamilieytelserPeriode = (
-  v: Validation,
-  namespace: string,
-  {
-    periode,
-    perioder,
-    nsIndex,
-    personName
-  }: ValidationFamilieytelsePeriodeProps
-): boolean => {
-  const hasErrors: Array<boolean> = []
-
-  const isPensjonPeriode = (p: Periode | PensjonPeriode | null | undefined): boolean => (
-    p
-      ? Object.prototype.hasOwnProperty.call(p, 'periode') || Object.prototype.hasOwnProperty.call(p, 'pensjonstype')
-      : false
-  )
-
-  const _periode: Periode = isPensjonPeriode(periode)
-    ? (periode as PensjonPeriode).periode
-    : (periode as Periode)
-
-  hasErrors.push(checkIfNotEmpty(v, {
-    needle: _periode?.__type,
-    id: namespace + (nsIndex ?? '') + '-type',
-    message: 'validation:noType',
-    personName
-  }))
-
-  if (_periode?.__type === 'perioderMedPensjon') {
-    hasErrors.push(checkIfNotEmpty(v, {
-      needle: (periode as PensjonPeriode).pensjonstype,
-      id: namespace + (nsIndex ?? '') + '-pensjonstype',
-      message: 'validation:noPensjonType',
-      personName
-    }))
-  }
-
-  hasErrors.push(validatePeriode(v, namespace + (nsIndex ?? ''), {
-    periode: _periode,
-    personName
-  }))
-
-  let haystack: Array<Periode | PensjonPeriode> | undefined
-
-  // check if the item is itself in the list, use a list without it, for proper duplicate check
-  if (_.isEmpty(nsIndex)) {
-    haystack = perioder
-  } else {
-    haystack = _.reject(perioder, (p: Periode | PensjonPeriode) => {
-      const _p: Periode = isPensjonPeriode(p) ? (p as PensjonPeriode).periode : (p as Periode)
-      return getNSIdx(_p.__type, _p.__index) === nsIndex
-    })
-  }
-
-  const duplicate = _.find(haystack, (p: Periode | PensjonPeriode) => {
-    return isPensjonPeriode(p)
-      ? (p as PensjonPeriode).periode.startdato === _periode?.startdato && (p as PensjonPeriode).periode.sluttdato === _periode?.sluttdato
-      : (p as Periode).startdato === _periode?.startdato && (p as Periode).sluttdato === _periode?.sluttdato
-  }) !== undefined
-
-  if (duplicate) {
-    hasErrors.push(addError(v, {
-      id: namespace + (nsIndex ?? '') + '-startdato',
-      message: 'validation:duplicateStartdato',
-      personName
-    }))
-  }
-
-  return hasErrors.find(value => value) !== undefined
-}
-
 export const validateTrygdeordning = (
   v: Validation,
   namespace: string,
   type: string,
-  perioder: Array<Periode | PensjonPeriode>,
+  perioder: Array<Periode>,
   personName?: string
 ): boolean => {
   const hasErrors: Array<boolean> = []
-  perioder?.forEach((periode: Periode | PensjonPeriode, index: number) => {
+  perioder?.forEach((periode: Periode, index: number) => {
     // in validation run from mainValidation, periode does not have __type, but in
     // new/edit periode validations, I must check __type, so let's fill it if it doesn't have
 
-    if (type === 'perioderMedPensjon') {
-      if (!Object.prototype.hasOwnProperty.call((periode as PensjonPeriode).periode, '__type')) {
-        (periode as PensjonPeriode).periode.__type = type;
-        (periode as PensjonPeriode).periode.__index = index
-      }
-    } else {
-      if (!Object.prototype.hasOwnProperty.call((periode as Periode), '__type')) {
-        (periode as Periode).__type = type;
-        (periode as Periode).__index = index
-      }
+    if (!Object.prototype.hasOwnProperty.call((periode as Periode), '__type')) {
+      (periode as Periode).__type = type;
+      (periode as Periode).__index = index
     }
 
-    if (type === 'perioderMedPensjon') {
-      hasErrors.push(validateFamilieytelserPeriode(v, namespace, {
-        periode: (periode as PensjonPeriode),
-        perioder: (perioder as Array<PensjonPeriode>),
-        nsIndex: getNSIdx(type, index),
-        personName
-      }))
 
-      delete (periode as PensjonPeriode).periode.__type
-      delete (periode as PensjonPeriode).periode.__index
+    hasErrors.push(validateDekkedePeriode(v, namespace, {
+      periode: (periode as Periode),
+      perioder: (perioder as Array<Periode>),
+      nsIndex: getNSIdx(type, index),
+      personName
+    }))
 
-    } else {
-      hasErrors.push(validateDekkedePeriode(v, namespace, {
-        periode: (periode as Periode),
-        perioder: (perioder as Array<Periode>),
-        nsIndex: getNSIdx(type, index),
-        personName
-      }))
+    delete (periode as Periode).__type
+    delete (periode as Periode).__index
 
-      delete (periode as Periode).__type
-      delete (periode as Periode).__index
-    }
   })
 
 
@@ -213,10 +116,7 @@ export const validateTrygdeordninger = (
   const hasErrors: Array<boolean> = []
   hasErrors.push(validateTrygdeordning(v, namespace, 'perioderMedITrygdeordning', _.get(replySed, `${personID}.perioderMedITrygdeordning`), personName))
   hasErrors.push(validateTrygdeordning(v, namespace, 'perioderUtenforTrygdeordning', _.get(replySed, `${personID}.perioderUtenforTrygdeordning`), personName))
-  hasErrors.push(validateTrygdeordning(v, namespace, 'perioderMedArbeid', _.get(replySed, `${personID}.perioderMedArbeid`), personName))
-  hasErrors.push(validateTrygdeordning(v, namespace, 'perioderMedTrygd', _.get(replySed, `${personID}.perioderMedTrygd`), personName))
   hasErrors.push(validateTrygdeordning(v, namespace, 'perioderMedYtelser', _.get(replySed, `${personID}.perioderMedYtelser`), personName))
-  hasErrors.push(validateTrygdeordning(v, namespace, 'perioderMedPensjon', _.get(replySed, `${personID}.perioderMedPensjon`), personName))
 
   if(isF026Sed(replySed)){
     const perioderMedYtelser: Array<Periode> | undefined = _.get(replySed, `${personID}.perioderMedYtelser`)
