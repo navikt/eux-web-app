6ae45b8d23366f679d640b9857c1abefca750feb
Knut Bjørnar Wålberg <knut.bjornar.walberg@nav.no>
2025-08-11 19:03:26 +0200

    Fornye token automatisk

--- src/mocks/app/utgaarDato.ts
+++ src/mocks/app/utgaarDato.ts
@@ -1,14 +1,19 @@
+const date = new Date();
+date.setTime(new Date().getTime() + 3600 * 1000 * 10);
+const date2 = new Date();
+date2.setTime(new Date().getTime() + 3600 * 1000 * 8);
+
 export default {
     session: {
       created_at: "2025-08-04T10:43:45.571915622Z",
-      ends_at: "2025-08-04T20:43:45.571915622Z",
+      ends_at: date.toISOString(),
       timeout_at: "0001-01-01T00:00:00Z",
       ends_in_seconds: 26417,
       active: true,
       timeout_in_seconds: -1
     },
     tokens: {
-      expire_at: "2025-08-04T14:43:03.860879157Z",
+      expire_at: date2.toISOString(),
       refreshed_at: "2025-08-04T13:23:20.860879157Z",
       expire_in_seconds: 4775,
       next_auto_refresh_in_seconds: 4475,
--- src/reducers/app.test.ts
+++ src/reducers/app.test.ts
@@ -41,11 +41,53 @@ describe('reducers/app', () => {
     })
   })
 
+  interface Session {
+    created_at: string;
+    ends_at: string;
+    timeout_at: string;
+    ends_in_seconds: number;
+    active: boolean;
+    timeout_in_seconds: number;
+  }
+
+  interface Tokens {
+    expire_at: string;
+    refreshed_at: string;
+    expire_in_seconds: number;
+    next_auto_refresh_in_seconds: number;
+    refresh_cooldown: boolean;
+    refresh_cooldown_seconds: number;
+  }
+
+  interface MockPayload {
+    session: Session
+    tokens: Tokens
+  }
+
   it('APP_UTGAARDATO_SUCCESS', () => {
-    const mockDate = new Date('2020-01-01 10:00:00')
-    const mockPayload = {
-      utgaarDato: mockDate
+    const mockDate: Date = new Date();
+    mockDate.setTime(new Date().getTime() + 3600 * 1000 * 8)
+    const mockDate2 = new Date();
+    mockDate2.setTime(new Date().getTime() + 3600 * 1000 * 10)
+    const mockPayload: MockPayload = {
+      session: {
+        created_at: "2025-08-04T10:43:45.571915622Z",
+        ends_at: mockDate2.toISOString(),
+        timeout_at: "0001-01-01T00:00:00Z",
+        ends_in_seconds: 26417,
+        active: true,
+        timeout_in_seconds: -1
+      },
+      tokens: {
+        expire_at: mockDate.toISOString(),
+        refreshed_at: "2025-08-04T13:23:20.860879157Z",
+        expire_in_seconds: 4775,
+        next_auto_refresh_in_seconds: 4475,
+        refresh_cooldown: true,
+        refresh_cooldown_seconds: 52
+      }
     }
+
     expect(
       appReducer(initialAppState, {
         type: types.APP_UTGAARDATO_SUCCESS,
@@ -56,6 +98,8 @@ describe('reducers/app', () => {
       expirationTime: mockDate
     })
   })
+
+
 
   it('APP_LOGMEAGAIN_SUCCESS', () => {
     const mockPayload = {
