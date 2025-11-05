f05b68cb63590bea3cbd65183e85ba99ef338532
Rittik Dey <dey.rittik@nav.no>
2025-11-05 09:25:36 +0100

    TEN-1369: Edit Motregning. Do not change sorting order of dates are equal. PeriodeInput as HGrid support

--- src/utils/sort.ts
+++ src/utils/sort.ts
@@ -7,17 +7,35 @@ const isDateValidFormat = (date: string, format:string) => {
 
 export const periodeSort = (a: Periode, b: Periode) => {
   if(isDateValidFormat(a.startdato, 'YYYY-MM-DD') && isDateValidFormat(b.startdato, 'YYYY-MM-DD')){
-    return moment(a.startdato, 'YYYY-MM-DD').isSameOrBefore(moment(b.startdato, 'YYYY-MM-DD')) ? -1 : 1
+    const dateA = moment(a.startdato, 'YYYY-MM-DD')
+    const dateB = moment(b.startdato, 'YYYY-MM-DD')
+    if(dateA.isSame(dateB)) return 0
+    return dateA.isBefore(dateB) ? -1 : 1
   } else if(isDateValidFormat(a.startdato, 'DD.MM.YYYY') && isDateValidFormat(b.startdato, 'DD.MM.YYYY')){
-    return moment(a.startdato, 'DD.MM.YYYY').isSameOrBefore(moment(b.startdato, 'DD.MM.YYYY')) ? -1 : 1
+    const dateA = moment(a.startdato, 'DD.MM.YYYY')
+    const dateB = moment(b.startdato, 'DD.MM.YYYY')
+    if(dateA.isSame(dateB)) return 0
+    return dateA.isBefore(dateB) ? -1 : 1
   } else if(isDateValidFormat(a.startdato, 'DD.MM.YYYY') && isDateValidFormat(b.startdato, 'YYYY-MM-DD')){
-    return moment(a.startdato, 'DD.MM.YYYY').isSameOrBefore(moment(b.startdato, 'YYYY-MM-DD')) ? -1 : 1
+    const dateA = moment(a.startdato, 'DD.MM.YYYY')
+    const dateB = moment(b.startdato, 'YYYY-MM-DD')
+    if(dateA.isSame(dateB)) return 0
+    return dateA.isBefore(dateB) ? -1 : 1
   } else if(isDateValidFormat(a.startdato, 'YYYY-MM-DD') && isDateValidFormat(b.startdato, 'DD.MM.YYYY')){
-    return moment(a.startdato, 'YYYY-MM-DD').isSameOrBefore(moment(b.startdato, 'DD.MM.YYYY')) ? -1 : 1
+    const dateA = moment(a.startdato, 'YYYY-MM-DD')
+    const dateB = moment(b.startdato, 'DD.MM.YYYY')
+    if(dateA.isSame(dateB)) return 0
+    return dateA.isBefore(dateB) ? -1 : 1
   }
-  return moment(a.startdato, 'YYYY-MM-DD').isSameOrBefore(moment(b.startdato, 'YYYY-MM-DD')) ? -1 : 1
+  const dateA = moment(a.startdato, 'YYYY-MM-DD')
+  const dateB = moment(b.startdato, 'YYYY-MM-DD')
+  if(dateA.isSame(dateB)) return 0
+  return dateA.isBefore(dateB) ? -1 : 1
 }
 
-export const periodePeriodeSort = (a: PeriodePeriode, b: PeriodePeriode) => (
-  moment(a.periode.startdato, 'YYYY-MM-DD').isSameOrBefore(moment(b.periode.startdato, 'YYYY-MM-DD')) ? -1 : 1
-)
+export const periodePeriodeSort = (a: PeriodePeriode, b: PeriodePeriode) => {
+  const dateA = moment(a.periode.startdato, 'YYYY-MM-DD')
+  const dateB = moment(b.periode.startdato, 'YYYY-MM-DD')
+  if(dateA.isSame(dateB)) return 0
+  return dateA.isBefore(dateB) ? -1 : 1
+}
