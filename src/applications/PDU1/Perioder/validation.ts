38c49cdc942d6ac7ab0f26e68ee678d548b1b3ea
Rittik Dey <dey.rittik@nav.no>
2025-12-15 13:27:00 +0100

    TEN-1393: Remove max limit validation  on periods for PDU1

--- src/applications/PDU1/Perioder/validation.ts
+++ src/applications/PDU1/Perioder/validation.ts
@@ -1,8 +1,7 @@
 import { PDPeriode, PDU1 } from 'declarations/pd.d'
 import { Validation } from 'declarations/types'
-import i18n from 'i18n'
 import { getNSIdx } from 'utils/namespace'
-import { addError, checkIfNotEmpty } from 'utils/validation'
+import { checkIfNotEmpty } from 'utils/validation'
 
 export interface ValidationPDPeriodeProps {
   periode: PDPeriode | undefined
@@ -66,44 +65,6 @@ export const validatePDPerioder = (
     delete periode.__type
     delete periode.__index
   })
-
-  let max: number = 0
-  switch (type) {
-    case 'perioderAnsattMedForsikring':
-      max = 20
-      break
-    case 'perioderSelvstendigMedForsikring':
-      max = 13
-      break
-    case 'perioderAndreForsikringer':
-      max = 7
-      break
-    case 'perioderAnsettSomForsikret':
-    case 'perioderAnsattUtenForsikring' :
-      max = 3
-      break
-    case 'perioderSelvstendigUtenForsikring':
-      max = 5
-      break
-    case 'perioderLoennSomAnsatt':
-    case 'perioderInntektSomSelvstendig':
-      max = 30
-      break
-    default:
-      break
-  }
-
-  if (perioder && perioder.length > max) {
-    addError(validation, {
-      id: namespace + '-generic',
-      message: 'validation:tooManyPeriodsMaxIs',
-      extra: {
-        type: i18n.t('el:option-perioder-' + type),
-        max: '' + max
-      }
-    })
-    hasErrors.push(true)
-  }
 
   return hasErrors.find(value => value) !== undefined
 }
