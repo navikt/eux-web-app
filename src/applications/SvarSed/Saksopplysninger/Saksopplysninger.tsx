352016d5b7f65f870af8e80c19df9ee84aa4b54b
Rittik Dey <dey.rittik@nav.no>
2026-01-14 13:44:21 +0100

    TEN-1121: SEDView/Saksopplysninger: Hoykontrast --> Aksel, Modal --> Dialog

--- src/applications/SvarSed/Saksopplysninger/Saksopplysninger.tsx
+++ src/applications/SvarSed/Saksopplysninger/Saksopplysninger.tsx
@@ -1,10 +1,9 @@
-import {BodyLong, Button, Heading, Modal, Panel} from '@navikt/ds-react'
-import { VerticalSeparatorDiv } from '@navikt/hoykontrast'
+import {BodyLong, Box, Button, Dialog, Heading, VStack} from '@navikt/ds-react'
 import AddMottakereModal from 'applications/SvarSed/AddMottakereModal/AddMottakereModal'
 import { Dd, Dl, Dt, HorizontalLineSeparator } from 'components/StyledComponents'
 import { Sak } from 'declarations/types'
 import _ from 'lodash'
-import React, {useRef} from 'react'
+import React, {useState} from 'react'
 import { useTranslation } from 'react-i18next'
 
 interface SaksopplysningerProps {
@@ -13,57 +12,62 @@ interface SaksopplysningerProps {
 
 const Saksopplysninger = ({ sak }: SaksopplysningerProps) => {
   const { t } = useTranslation()
-  const ref = useRef<HTMLDialogElement>(null);
+  const [_dialogOpen, setDialogOpen] = useState<boolean>(false)
 
   const canChangeParticipants = _.find(sak.sakshandlinger, s => s === "singleParticipant" || s === "multipleParticipants")  !== undefined
   const type = sak.sakshandlinger?.includes("multipleParticipants") ? "multiple" : "single"
 
   return (
     <>
-      <Modal ref={ref} header={{ heading: type === "multiple" ? t('label:add-deltakere-modal') : t('label:add-deltaker-modal') }} width="medium" style={{overflow: "visible"}}>
-        <Modal.Body style={{overflow: "visible"}}>
-          <AddMottakereModal
-            bucType={sak.sakType}
-            rinaSakId={sak.sakId}
-            sakshandlinger={sak.sakshandlinger}
-            onClose={() => ref.current?.close()}
-          />
-        </Modal.Body>
-      </Modal>
-      <Panel border>
-        <Heading size='small'>
-          {t('label:saksopplysninger')}
-        </Heading>
-        <VerticalSeparatorDiv />
-        <HorizontalLineSeparator />
-        <VerticalSeparatorDiv />
-        <Dl>
-          <Dt>
-            {t('label:vår-rolle')}:
-          </Dt>
-          <Dd>
-            {sak.erSakseier && t('label:sakseier')}
-            {!sak.erSakseier && t('label:motpart')}
-            {_.isNil(sak.erSakseier) && t('label:ukjent')}
-          </Dd>
-          <Dt>
-            {t('label:andre-deltakere')}:
-          </Dt>
-          <Dd>
-            {sak.motpart?.map(m => <BodyLong key={m}>{m}</BodyLong>)}
-          </Dd>
-        </Dl>
-        <VerticalSeparatorDiv />
-        {canChangeParticipants && (
-          <Button
-            variant='secondary'
-            onClick={() => ref.current?.showModal()}
-          >
-            {t('el:button-change-participants')}
-          </Button>
-        )}
-
-      </Panel>
+      <Dialog open={_dialogOpen} onOpenChange={()=> setDialogOpen(!_dialogOpen)}>
+        <Dialog.Popup style={{overflow: "visible"}}>
+          <Dialog.Header>
+            <Dialog.Title>{type === "multiple" ? t('label:add-deltakere-modal') : t('label:add-deltaker-modal')}</Dialog.Title>
+          </Dialog.Header>
+          <Dialog.Body style={{overflow: "visible"}}>
+            <AddMottakereModal
+              bucType={sak.sakType}
+              rinaSakId={sak.sakId}
+              sakshandlinger={sak.sakshandlinger}
+              onClose={() => setDialogOpen(false)}
+            />
+          </Dialog.Body>
+        </Dialog.Popup>
+      </Dialog>
+      <Box padding="4" background="bg-default" borderWidth="1" borderColor="border-default" borderRadius="small">
+        <VStack>
+          <VStack gap="4">
+            <Heading size='small'>
+              {t('label:saksopplysninger')}
+            </Heading>
+            <HorizontalLineSeparator />
+          </VStack>
+          <Dl>
+            <Dt>
+              {t('label:vår-rolle')}:
+            </Dt>
+            <Dd>
+              {sak.erSakseier && t('label:sakseier')}
+              {!sak.erSakseier && t('label:motpart')}
+              {_.isNil(sak.erSakseier) && t('label:ukjent')}
+            </Dd>
+            <Dt>
+              {t('label:andre-deltakere')}:
+            </Dt>
+            <Dd>
+              {sak.motpart?.map(m => <BodyLong key={m}>{m}</BodyLong>)}
+            </Dd>
+          </Dl>
+          {canChangeParticipants && (
+            <Button
+              variant='secondary'
+              onClick={() => setDialogOpen(true)}
+            >
+              {t('el:button-change-participants')}
+            </Button>
+          )}
+        </VStack>
+      </Box>
     </>
   )
 }
