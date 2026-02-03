1229b6f3da2b0a604cad08275f27b85f40e68e14
Rittik Dey <dey.rittik@nav.no>
2026-02-03 15:29:37 +0100

    TEN-1121: Ugyldiggjøre: Hoykontrast --> Aksel.

--- src/applications/SvarSed/Ugyldiggjøre/Ugyldiggjøre.tsx
+++ src/applications/SvarSed/Ugyldiggjøre/Ugyldiggjøre.tsx
@@ -1,12 +1,4 @@
-import { Heading } from '@navikt/ds-react'
-import {
-  AlignStartRow,
-  Column,
-  PaddedDiv,
-  RadioPanel,
-  RadioPanelGroup,
-  VerticalSeparatorDiv
-} from '@navikt/hoykontrast'
+import { Box, Heading, HGrid, Radio, RadioGroup, VStack } from '@navikt/ds-react'
 import { resetValidation, setValidation } from 'actions/validation'
 import { MainFormProps, MainFormSelector } from 'applications/SvarSed/MainForm'
 import Input from 'components/Forms/Input'
@@ -19,6 +11,7 @@ import { useTranslation } from 'react-i18next'
 import { useAppDispatch, useAppSelector } from 'store'
 import performValidation from 'utils/performValidation'
 import { validateUgyldiggjøre, ValidationUgyldiggjøreProps } from './validation'
+import commonStyles from 'assets/css/common.module.css'
 
 const mapState = (state: State): MainFormSelector => ({
   validation: state.validation.status
@@ -66,55 +59,47 @@ const Ugyldiggjøre: React.FC<MainFormProps> = ({
   }
 
   return (
-    <PaddedDiv>
-      <Heading size='small'>
-        {label} ID: {(replySed as X008Sed)?.kansellerSedId} ({(replySed as X008Sed)?.utstedelsesdato})
-      </Heading>
-      <VerticalSeparatorDiv size='2' />
-
-      <AlignStartRow>
-        <Column flex='2'>
-          <RadioPanelGroup
+    <Box padding="4">
+      <VStack gap="4">
+        <Heading size='small'>
+          {label} ID: {(replySed as X008Sed)?.kansellerSedId} ({(replySed as X008Sed)?.utstedelsesdato})
+        </Heading>
+        <HGrid columns={"2fr 1fr"} gap="4" align="start">
+          <RadioGroup
             value={(replySed as X008Sed).begrunnelseType}
-            data-no-border
             data-testid={namespace + '-begrunnelseType'}
             error={validation[namespace + '-begrunnelseType']?.feilmelding}
             id={namespace + '-begrunnelseType'}
             legend={t('label:begrunnelse')}
-            hideLabel={false}
-            required
-            name={namespace + '-begrunnelseType'}
             onChange={setBegrunnelseType}
           >
-            <RadioPanel value='personen_er_død'>{t('el:option-ugyldiggjøre-01')}</RadioPanel>
-            <RadioPanel value='saken_ble_feilaktig_sendt_til_dere'>{t('el:option-ugyldiggjøre-02')}</RadioPanel>
-            <RadioPanel value='feilaktig_informasjon_levert'>{t('el:option-ugyldiggjøre-03')}</RadioPanel>
-            <RadioPanel value='saken_ble_revurdert_og_sed_en_er_ikke_lenger_vurdert_som_ugyldig'>{t('el:option-ugyldiggjøre-04')}</RadioPanel>
-            <RadioPanel value='det_nasjonale_vedtaket_ble_bestridt_av_kunden_Mer_informasjon_vil_følge_etter_endelig_beslutning_om_bestridelsen'>{t('el:option-ugyldiggjøre-05')}</RadioPanel>
-            <RadioPanel value='det_nasjonale_vedtaket_ble_bestridt_av_kunden_Det_vil_ikke_sendes_flere_sed_er'>{t('el:option-ugyldiggjøre-06')}</RadioPanel>
-            <RadioPanel value='annet'>{t('el:option-ugyldiggjøre-99')}</RadioPanel>
-          </RadioPanelGroup>
-        </Column>
-        <Column />
-      </AlignStartRow>
-      <VerticalSeparatorDiv />
-      {(replySed as X008Sed).begrunnelseType === 'annet' && (
-        <AlignStartRow>
-          <Column>
-            <Input
-              error={validation[namespace + '-begrunnelseAnnen']?.feilmelding}
-              namespace={namespace}
-              id='begrunnelseAnnen'
-              label={t('label:begrunnelseAnnen')}
-              hideLabel
-              onChanged={setBegrunnelseAnnen}
-              required
-              value={(replySed as X008Sed).begrunnelseAnnen}
-            />
-          </Column>
-        </AlignStartRow>
-      )}
-    </PaddedDiv>
+            <VStack gap="1">
+              <Radio className={commonStyles.radioPanel} value='personen_er_død'>{t('el:option-ugyldiggjøre-01')}</Radio>
+              <Radio className={commonStyles.radioPanel} value='saken_ble_feilaktig_sendt_til_dere'>{t('el:option-ugyldiggjøre-02')}</Radio>
+              <Radio className={commonStyles.radioPanel} value='feilaktig_informasjon_levert'>{t('el:option-ugyldiggjøre-03')}</Radio>
+              <Radio className={commonStyles.radioPanel} value='saken_ble_revurdert_og_sed_en_er_ikke_lenger_vurdert_som_ugyldig'>{t('el:option-ugyldiggjøre-04')}</Radio>
+              <Radio className={commonStyles.radioPanel} value='det_nasjonale_vedtaket_ble_bestridt_av_kunden_Mer_informasjon_vil_følge_etter_endelig_beslutning_om_bestridelsen'>{t('el:option-ugyldiggjøre-05')}</Radio>
+              <Radio className={commonStyles.radioPanel} value='det_nasjonale_vedtaket_ble_bestridt_av_kunden_Det_vil_ikke_sendes_flere_sed_er'>{t('el:option-ugyldiggjøre-06')}</Radio>
+              <Radio className={commonStyles.radioPanel} value='annet'>{t('el:option-ugyldiggjøre-99')}</Radio>
+            </VStack>
+          </RadioGroup>
+          <div />
+        </HGrid>
+
+        {(replySed as X008Sed).begrunnelseType === 'annet' && (
+          <Input
+            error={validation[namespace + '-begrunnelseAnnen']?.feilmelding}
+            namespace={namespace}
+            id='begrunnelseAnnen'
+            label={t('label:begrunnelseAnnen')}
+            hideLabel
+            onChanged={setBegrunnelseAnnen}
+            required
+            value={(replySed as X008Sed).begrunnelseAnnen}
+          />
+        )}
+      </VStack>
+    </Box>
   )
 }
 
