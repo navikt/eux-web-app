490937c3dfeccb8a9efcc05c38253e94b322ebce
Rittik Dey <dey.rittik@nav.no>
2026-02-09 13:01:54 +0100

    TEN-1121: SvarPaaForespoerselOmAdopsjon - Høykontrast --> Aksel

--- src/applications/SvarSed/SvarPaaForespoerselOmAdopsjon/SvarPaaForespoerselOmAdopsjon.tsx
+++ src/applications/SvarSed/SvarPaaForespoerselOmAdopsjon/SvarPaaForespoerselOmAdopsjon.tsx
@@ -1,11 +1,9 @@
-import {VStack, Box, Heading} from '@navikt/ds-react'
-import {Column, Row} from '@navikt/hoykontrast'
+import {VStack, Box, Heading, HGrid} from '@navikt/ds-react'
 import { MainFormProps, MainFormSelector } from 'applications/SvarSed/MainForm'
 import { State } from 'declarations/reducers'
 import _ from 'lodash'
 import React from 'react'
 import { useAppDispatch, useAppSelector } from 'store'
-import {TextAreaDiv} from "../../../components/StyledComponents";
 import TextArea from "../../../components/Forms/TextArea";
 import {SvarAdopsjon} from "../../../declarations/sed";
 import DateField from "../../../components/DateField/DateField";
@@ -54,40 +52,34 @@ const SvarPaaForespoerselOmAdopsjon: React.FC<MainFormProps> = ({
           {label}
         </Heading>
         <Box padding="4" background="surface-subtle" borderWidth="1" borderColor="border-subtle">
-          <Row>
-            <Column flex={1}>
-              <DateField
-                error={validation[namespace + '-adoptivforeldreOmsorgFradato']?.feilmelding}
-                namespace={namespace}
-                id='adoptivforeldreOmsorgFradato'
-                label={t('label:dato-adoptivforeldre-omsorg-fradato')}
-                onChanged={(v) => setAdopsjonProperty('adoptivforeldreOmsorgFradato', v)}
-                dateValue={svarAdopsjon?.adoptivforeldreOmsorgFradato}
-              />
-            </Column>
-            <Column flex={1}>
-              <DateField
-                error={validation[namespace + '-bevillingRegistreringsdato']?.feilmelding}
-                namespace={namespace}
-                id='bevillingRegistreringsdato'
-                label={t('label:dato-bevilling-registrering')}
-                onChanged={(v) => setAdopsjonProperty('bevillingRegistreringsdato', v)}
-                dateValue={svarAdopsjon?.bevillingRegistreringsdato}
-              />
-            </Column>
-          </Row>
+          <HGrid columns={2} gap="4" align="start">
+            <DateField
+              error={validation[namespace + '-adoptivforeldreOmsorgFradato']?.feilmelding}
+              namespace={namespace}
+              id='adoptivforeldreOmsorgFradato'
+              label={t('label:dato-adoptivforeldre-omsorg-fradato')}
+              onChanged={(v) => setAdopsjonProperty('adoptivforeldreOmsorgFradato', v)}
+              dateValue={svarAdopsjon?.adoptivforeldreOmsorgFradato}
+            />
+            <DateField
+              error={validation[namespace + '-bevillingRegistreringsdato']?.feilmelding}
+              namespace={namespace}
+              id='bevillingRegistreringsdato'
+              label={t('label:dato-bevilling-registrering')}
+              onChanged={(v) => setAdopsjonProperty('bevillingRegistreringsdato', v)}
+              dateValue={svarAdopsjon?.bevillingRegistreringsdato}
+            />
+          </HGrid>
         </Box>
         <Box padding="4" background="surface-subtle" borderWidth="1" borderColor="border-subtle">
-          <TextAreaDiv>
-            <TextArea
-              error={validation[namespace + '-ytterligereInfo']?.feilmelding}
-              namespace={namespace}
-              id='ytterligereInfo'
-              label={t('label:extra-information')}
-              onChanged={(v) => setAdopsjonProperty('ytterligereInformasjon', v)}
-              value={svarAdopsjon?.ytterligereInformasjon}
-            />
-          </TextAreaDiv>
+          <TextArea
+            error={validation[namespace + '-ytterligereInfo']?.feilmelding}
+            namespace={namespace}
+            id='ytterligereInfo'
+            label={t('label:extra-information')}
+            onChanged={(v) => setAdopsjonProperty('ytterligereInformasjon', v)}
+            value={svarAdopsjon?.ytterligereInformasjon}
+          />
         </Box>
       </VStack>
     </Box>
