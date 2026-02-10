e02b66103bba07ccd15a69fc691d8168c157aeb7
Rittik Dey <dey.rittik@nav.no>
2026-02-10 15:34:31 +0100

    TEN-1121: Error: Høykontrast --> Aksel

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,38 +1,17 @@
 import Convert from 'ansi-to-html'
 import classNames from 'classnames'
 import TopContainer from 'components/TopContainer/TopContainer'
-import { Accordion, BodyLong } from '@navikt/ds-react'
-import { VerticalSeparatorDiv } from '@navikt/hoykontrast'
+import {Accordion, BodyLong, Page, VStack} from '@navikt/ds-react'
 import React  from 'react'
 import { useTranslation } from 'react-i18next'
 import StackTracey from 'stacktracey'
-import styled from 'styled-components'
+import styles from './Error.module.css'
 
 const convert = new Convert({
   newline: true,
   escapeXML: true,
   fg: '#000'
 })
-
-const Content = styled.div`
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-`
-const Description = styled.div`
-  width: 80%;
-  margin: 1rem;
-  text-align: center;
-`
-
-const Line = styled.div`
-  width: 60%;
-  margin: 1rem;
-  min-height: 0.25rem;
-  border-bottom: 1px solid #78706A;
-`
 
 export interface ErrorProps {
   error?: any;
@@ -49,33 +28,34 @@ export const Error = ({ error }: ErrorProps) => {
   const msg = convert.toHtml(error.message)
 
   return (
-    <TopContainer title={title} className={classNames('p-error')}>
-      <Content>
-        <Description dangerouslySetInnerHTML={{ __html: description }} />
-        {error && (
-          <Accordion>
-            <Accordion.Item defaultOpen>
-              <Accordion.Header>{t('message:error-header')}</Accordion.Header>
-              <Accordion.Content>
-                <div style={{ whiteSpace: 'break-spaces' }} dangerouslySetInnerHTML={{ __html: msg }} />
-                {stack.items?.map((e: any, i) => (
-                  <div key={'' + i}>{e?.beforeParse}</div>
-                ))}
-              </Accordion.Content>
-            </Accordion.Item>
-          </Accordion>
-        )}
-        {footer && (
-          <>
-            <Line />
-            <VerticalSeparatorDiv size='0.5' />
-            <BodyLong>
-              {footer}
-            </BodyLong>
-          </>
-        )}
-      </Content>
-    </TopContainer>
+    <Page>
+      <TopContainer title={title} className={classNames('p-error')}>
+        <Page.Block width="2xl" as="main">
+          <div className={styles.description} dangerouslySetInnerHTML={{ __html: description }} />
+          {error && (
+            <Accordion>
+              <Accordion.Item defaultOpen>
+                <Accordion.Header>{t('message:error-header')}</Accordion.Header>
+                <Accordion.Content>
+                  <div style={{ whiteSpace: 'break-spaces' }} dangerouslySetInnerHTML={{ __html: msg }} />
+                  {stack.items?.map((e: any, i) => (
+                    <div key={'' + i}>{e?.beforeParse}</div>
+                  ))}
+                </Accordion.Content>
+              </Accordion.Item>
+            </Accordion>
+          )}
+          {footer && (
+            <VStack gap="2">
+              <div className={styles.line}/>
+              <BodyLong>
+                {footer}
+              </BodyLong>
+            </VStack>
+          )}
+        </Page.Block>
+      </TopContainer>
+    </Page>
   )
 }
 
