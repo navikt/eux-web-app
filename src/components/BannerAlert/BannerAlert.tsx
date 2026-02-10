7c4c3ba4440582d7e8c84c0f48b122fd3154da81
Rittik Dey <dey.rittik@nav.no>
2026-02-10 13:33:08 +0100

    TEN-1121: BannerAlert: Høykontrast --> Aksel. Added animation-styles to CSS Module.

--- src/components/BannerAlert/BannerAlert.tsx
+++ src/components/BannerAlert/BannerAlert.tsx
@@ -1,35 +1,17 @@
 import { XMarkOctagonFillIcon } from '@navikt/aksel-icons'
 import classNames from 'classnames'
-import { fadeIn, HorizontalSeparatorDiv } from '@navikt/hoykontrast'
 import { AlertError, AlertVariant } from 'declarations/components'
 import _ from 'lodash'
 import { Alert } from '@navikt/ds-react'
 import React from 'react'
-import styled from 'styled-components'
-
-export const AlertDiv = styled(Alert)`
-  opacity: 0;
-  animation: ${fadeIn} 1s forwards;
-  position: sticky;
-  top: 0;
-  z-index: 10;
-  width: 100%;
-  border-radius: 0px !important;
-  border: 0px !important;
-  .navds-alert__wrapper {
-    display: flex !important;
-  }
-`
-export const CloseIcon = styled(XMarkOctagonFillIcon)`
-  cursor: pointer;
-`
+import styles from './BannerAlert.module.css'
 
 export interface BannerAlertProps {
   className ?: string
   error?: AlertError | string
   message?: JSX.Element | string
   onClose?: () => void
-  variant: AlertVariant | undefined
+  variant: AlertVariant
   style ?: any
 }
 
@@ -70,26 +52,22 @@ export const BannerAlert: React.FC<BannerAlertProps> = ({
   }
 
   return (
-    <AlertDiv
-      data-testid='c-BannerAlert'
-      className={classNames('status-' + variant, className)}
+    <Alert
+      className={classNames(styles.bannerAlert, 'status-' + variant, className)}
       style={style}
       role='alert'
       variant={variant}
     >
       {_message}
       {onClose && (
-        <div>
-          <HorizontalSeparatorDiv />
-          <CloseIcon
-            height='24'
-            width='24'
-            data-testid='c-BannerAlert__CloseIcon'
-            onClick={onCloseIconClicked}
-          />
-        </div>
+        <XMarkOctagonFillIcon
+          className={styles.closeIcon}
+          fontSize="1.5rem"
+          data-testid='c-alert--close-icon'
+          onClick={onCloseIconClicked}
+        />
       )}
-    </AlertDiv>
+    </Alert>
   )
 }
 
