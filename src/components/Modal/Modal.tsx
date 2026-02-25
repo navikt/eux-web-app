369d5e21fcae4ffa589bd7beb6fdadacd657555b
Rittik Dey <dey.rittik@nav.no>
2026-02-25 11:17:30 +0100

    TEN-1473: Modal: Styled Components --> CSS Module

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,36 +1,9 @@
 import classNames from 'classnames'
-import {BodyShort, Button, Heading, Modal} from '@navikt/ds-react'
+import {BodyShort, Box, Button, Heading, HStack, Modal} from '@navikt/ds-react'
 import { ModalContent } from 'declarations/components'
 import _ from 'lodash'
 import React from 'react'
-
-import styled from 'styled-components'
-
-const ModalText = styled.div`
-  margin: 1.5rem;
-  text-align: center;
-`
-const ModalButtons = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-`
-const ButtonMargin = styled.div`
-  margin-right: 1rem;
-  margin-top: 0.5rem;
-  margin-botton: 0.5rem;
-`
-
-const ContentDiv = styled.div`
-  overflow: auto;
-  max-height: 85vh;
-  &.icon {
-    margin-top: 6rem;
-  }
-  &.buttons {
-    margin-top: 3rem;
-  }
-`
+import styles from './Modal.module.css'
 
 export interface ModalProps {
   className?: string
@@ -73,47 +46,48 @@ const ModalFC: React.FC<ModalProps> = ({
         </>
       </Modal.Header>
       <Modal.Body>
-        <ContentDiv className={classNames({ icon: !!icon })}>
+        <div className={classNames(styles.content, { [styles.icon]: !!icon })}>
           {modal?.modalContent || (
-            <ModalText data-testid='modal__text-id'>
+            <div className={styles.modaltext} data-testid='modal__text-id'>
               {modal?.modalText}
-            </ModalText>
+            </div>
           )}
-        </ContentDiv>
+        </div>
         {modal?.modalButtons && (
-          <ModalButtons className={classNames('buttons')}>
-            {modal?.modalButtons.map((button, i) => {
-              if (button.hide) {
-                return null
-              }
-              let variant  = button.variant
-              //let variant: 'tertiary' | 'primary' | 'secondary' | 'danger' | undefined = 'secondary'
-              if (button.main) {
-                variant = 'primary'
-              }
-              if (button.flat) {
-                variant = 'tertiary'
-              }
-              const handleClick = _.isFunction(button.onClick)
-                ? () => {
-                  button.onClick!()
-                  }
-                : onModalClose
-              return (
-                <ButtonMargin key={i}>
+          <Box padding="4">
+            <HStack justify="center" gap="4">
+              {modal?.modalButtons.map((button, i) => {
+                if (button.hide) {
+                  return null
+                }
+                let variant  = button.variant
+                //let variant: 'tertiary' | 'primary' | 'secondary' | 'danger' | undefined = 'secondary'
+                if (button.main) {
+                  variant = 'primary'
+                }
+                if (button.flat) {
+                  variant = 'tertiary'
+                }
+                const handleClick = _.isFunction(button.onClick)
+                  ? () => {
+                    button.onClick!()
+                    }
+                  : onModalClose
+                return (
                   <Button
                     variant={variant ? variant : 'secondary'}
                     data-testid={'modal__button-id-' + i}
                     disabled={button.disabled || false}
                     id={'modal__button-id-' + i}
                     onClick={handleClick}
+                    key={i}
                   >
                     {button.text}
                   </Button>
-                </ButtonMargin>
-              )
-            })}
-          </ModalButtons>
+                )
+              })}
+            </HStack>
+          </Box>
         )}
       </Modal.Body>
     </Modal>
