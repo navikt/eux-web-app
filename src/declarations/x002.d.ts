2b8054f993208cf72489ede23e3c7e6a740ff077
Knut Bjørnar Wålberg <knut.bjornar.walberg@nav.no>
2026-05-12 10:22:30 +0200

    TEN-1582 - QA

--- src/declarations/x002.d.ts
+++ src/declarations/x002.d.ts
@@ -1,14 +1,10 @@
 import { XSed } from 'declarations/sed'
 
-// ===== X002 interfaces =====
-
-export interface X002Gjenaapning {
+export interface Gjenaapning {
   aarsakType?: string
   aarsakAnnet?: string
 }
 
-// ===== X002 SED =====
-
 export interface X002Sed extends XSed {
-  gjenaapning?: X002Gjenaapning
+  gjenaapning?: Gjenaapning
 }
