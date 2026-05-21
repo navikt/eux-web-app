ac4729de376831444dc4b53e5b6a75ac21a3c600
Knut Bjørnar Wålberg <knut.bjornar.walberg@nav.no>
2026-05-21 23:12:15 +0200

    TEN-1587 - X004 Gjenåpne sak

---
+++ src/applications/SvarSed/BekreftelseGjenaapning/BekreftelseGjenaapning.tsx
@@ -0,0 +1,101 @@
+import { Box, Heading, Radio, RadioGroup, VStack } from '@navikt/ds-react'
+import { resetValidation, setValidation } from 'actions/validation'
+import { MainFormProps, MainFormSelector } from 'applications/SvarSed/MainForm'
+import { State } from 'declarations/reducers'
+import { X004Sed } from 'declarations/x004'
+import useUnmount from 'hooks/useUnmount'
+import _ from 'lodash'
+import React, { JSX } from 'react'
+import { useTranslation } from 'react-i18next'
+import { useAppDispatch, useAppSelector } from 'store'
+import performValidation from 'utils/performValidation'
+import { validateBekreftelseGjenaapning, ValidationBekreftelseGjenaapningProps } from './validation'
+import commonStyles from 'assets/css/common.module.css'
+
+const mapState = (state: State): MainFormSelector => ({
+  validation: state.validation.status
+})
+
+const BekreftelseGjenaapning: React.FC<MainFormProps> = ({
+  label,
+  parentNamespace,
+  personID,
+  personName,
+  replySed,
+  updateReplySed
+}:MainFormProps): JSX.Element => {
+  const { validation } = useAppSelector(mapState)
+  const { t } = useTranslation()
+  const dispatch = useAppDispatch()
+  const namespace = `${parentNamespace}-${personID}-bekreftelsegjenaapning`
+  const sed = replySed as X004Sed
+
+  useUnmount(() => {
+    const clonedValidation = _.cloneDeep(validation)
+    performValidation<ValidationBekreftelseGjenaapningProps>(
+      clonedValidation, namespace, validateBekreftelseGjenaapning, {
+        replySed: sed,
+        personName
+      }
+    )
+    dispatch(setValidation(clonedValidation))
+  })
+
+  const setSkalGjenaapnes = (skalGjenaapnes: string) => {
+    dispatch(updateReplySed('gjenaapning.skalGjenaapnes', skalGjenaapnes.trim()))
+    if (skalGjenaapnes === 'ja') {
+      dispatch(updateReplySed('gjenaapning.grunnType', ''))
+    }
+    if (validation[namespace + '-skalGjenaapnes']) {
+      dispatch(resetValidation(namespace + '-skalGjenaapnes'))
+    }
+  }
+
+  const setGrunnType = (grunnType: string) => {
+    dispatch(updateReplySed('gjenaapning.grunnType', grunnType.trim()))
+    if (validation[namespace + '-grunnType']) {
+      dispatch(resetValidation(namespace + '-grunnType'))
+    }
+  }
+
+  return (
+    <Box padding="4">
+      <VStack gap="4">
+        <Heading size='small'>
+          {label}
+        </Heading>
+
+        <RadioGroup
+          value={sed.gjenaapning?.skalGjenaapnes ?? ''}
+          data-testid={namespace + '-skalGjenaapnes'}
+          error={validation[namespace + '-skalGjenaapnes']?.feilmelding}
+          id={namespace + '-skalGjenaapnes'}
+          legend={t('label:bekreftelsegjenaapning-skal-gjenaapnes')}
+          onChange={setSkalGjenaapnes}
+        >
+          <VStack gap="2">
+            <Radio className={commonStyles.radioPanel} value='ja'>{t('el:option-bekreftelsegjenaapning-ja')}</Radio>
+            <Radio className={commonStyles.radioPanel} value='nei'>{t('el:option-bekreftelsegjenaapning-nei')}</Radio>
+          </VStack>
+        </RadioGroup>
+
+        {sed.gjenaapning?.skalGjenaapnes === 'nei' && (
+          <RadioGroup
+            value={sed.gjenaapning?.grunnType ?? ''}
+            data-testid={namespace + '-grunnType'}
+            error={validation[namespace + '-grunnType']?.feilmelding}
+            id={namespace + '-grunnType'}
+            legend={t('label:bekreftelsegjenaapning-grunn')}
+            onChange={setGrunnType}
+          >
+            <VStack gap="2">
+              <Radio className={commonStyles.radioPanel} value='alle_deltakerne_kan_ikke_gjenåpne_saken'>{t('el:option-bekreftelsegjenaapning-grunn-01')}</Radio>
+            </VStack>
+          </RadioGroup>
+        )}
+      </VStack>
+    </Box>
+  )
+}
+
+export default BekreftelseGjenaapning
